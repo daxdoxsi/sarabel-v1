50a80a616ecb56db56531912956f6b4f336f2199
David M. Miller <66507498+daxdoxsi@users.noreply.github.com>
2020-11-21 17:13:28 -0600

    Delete asana-dashboard.ctl.php

--- app/views/templates/asana-dashboard.ctl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$tpl_modules = [
-    'top_nav'   => view('modules/asana-dashboard/top_nav', [], true),
-    'menu'      => view('modules/asana-dashboard/menu', [], true),
-    'footer'    => view('modules/asana-dashboard/footer', [], true)
-];
-
